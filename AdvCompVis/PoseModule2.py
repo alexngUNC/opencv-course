f9d6bb1c0a1df65d534c882bda15b81ab413cf10
Alex Georgiev <ageorgiev@unc.edu>
2023-08-14 17:29:53 -0400

    finished curl detector

--- AdvCompVis/PoseModule2.py
+++ AdvCompVis/PoseModule2.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import mediapipe as mp
 import time
 
@@ -24,27 +25,53 @@ class PoseDetector():
     return img
 
   def findPosition(self, img, draw=True):
-    lmList = []
+    self.lmList = []
     if self.results.pose_landmarks:
       for id, lm in enumerate(self.results.pose_landmarks.landmark):
         h, w, c = img.shape
         #print(id, lm)
         cx, cy = int(lm.x*w), int(lm.y*h)
-        lmList.append([id, cx, cy])
+        self.lmList.append([id, cx, cy])
         if draw:
           cv2.circle(img, (cx, cy), 10, (255,0,255), cv2.FILLED)
-    return lmList
+    return self.lmList
+  
+  def findAngle(self, img, p1, p2, p3, draw=True):
+    # get landmarks
+    x1, y1 = self.lmList[p1][1:]
+    x2, y2 = self.lmList[p2][1:]
+    x3, y3 = self.lmList[p3][1:]
+
+    # calculate angle bewteen landmarks
+    angle = abs(math.degrees(math.atan2(y3-y2, x3-x2) - math.atan2(y1-y2, x1-x2)))
+
+    #print(angle)
+
+    # draw
+    if draw:
+      cv2.line(img, (x1, y1), (x2, y2), (255, 255, 0), 3)
+      cv2.line(img, (x3, y3), (x2, y2), (255, 255, 0), 3)
+      cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+      cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+      cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+      cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+      cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+      cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+      cv2.putText(img, str(int(angle)), (x2-70, y2+50), cv2.FONT_HERSHEY_PLAIN,
+                  2, (255, 255, 0), 2)
+    return angle
+
 
 def main():
   #cap = cv2.VideoCapture('PoseVideos/1.mp4')
-  cap = cv2.VideoCapture(0)
+  cap = cv2.VideoCapture(1)
   pTime = 0
   detector = PoseDetector()
   while True:
     success, img = cap.read()
     img = detector.findPose(img, draw=True)
     lmList = detector.findPosition(img)
-    print(lmList)
+    # print(lmList)
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime=cTime
